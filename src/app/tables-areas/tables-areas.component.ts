af0728e52422a39ee919eef127a08b3fe93a7cbb
Da3l-Dev <daelmartinezsimon@gmail.com>
2025-02-14 15:54:07 -0600

    Implementacion de obtencion de datos en las tables

--- src/app/tables-areas/tables-areas.component.ts
+++ src/app/tables-areas/tables-areas.component.ts
@@ -5,6 +5,8 @@ import { response } from 'express';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Tag } from 'primeng/tag';
 import { SharedDataService } from '../services/shared-data.service';
+import { elementAt } from 'rxjs';
+import { log } from 'console';
 
 
 @Component({
@@ -20,15 +22,49 @@ import { SharedDataService } from '../services/shared-data.service';
 
 export class TablesAreasComponent implements OnInit{
   datos: any[] = [];
+  logros: any[] = [];
+  resultados: any[] = [];
+  totales: any[] =[];
 
+
+  total: number = 0;
   constructor (
     private dataService: ObtenerdatosService,
     private sharedData: SharedDataService
   ){}
-  ngOnInit(): void {
-    this.sharedData.arregloAreas$.subscribe(data => {
+  async ngOnInit(): Promise<void> {
+    try{
+      await this.obtenerDatos();
+      await this.conteoLogros(this.datos);
+
+      console.log(this.total);
+    }catch (error) {
+      console.error('Error al obtener los datos:', error);
+    }
+  }
+
+  async obtenerDatos(): Promise<void>{
+    this.sharedData.arregloAreas$.subscribe(async data => {
       this.datos = data;
     })
   }
 
+  async conteoLogros(logro: any): Promise<void>{ 
+    
+    this.datos.forEach(element => {
+      this.dataService.getDataLogros(element.idArea).subscribe(data => {
+        this.logros = data;
+
+        this.resultados = this.logros.filter(element => element.idTrimestre === 1);
+
+
+        this.resultados.forEach(element => {
+          if(element.causa != null){
+            this.total += 1;
+          }
+        });
+
+      });
+    });
+  }
 }
