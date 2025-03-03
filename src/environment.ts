70cf76768ded0e1d11ca5b7742d23ec30f054b6c
Da3l-Dev <daelmartinezsimon@gmail.com>
2025-03-03 15:12:44 -0600

    Implementacion de un menu desplegable

--- src/environment.ts
+++ src/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost' // URL de la API para desarrollo
+    apiUrl: 'http://localhost:8080/apiserver' // URL de la API para desarrollo
   };
   
